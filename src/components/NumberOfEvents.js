927ee8648d50720cb0635f4fbdd5dfca7e3c4dc9
mchays12 <mchays12@gmail.com>
2023-09-16 15:13:40 -0400

    updated error

--- src/components/NumberOfEvents.js
+++ src/components/NumberOfEvents.js
@@ -1,15 +1,15 @@
-const NumberOfEvents = ({ setCurrentNOE, setErrorAlert }) => {
+const NumberOfEvents = ({ setCurrentNOE }) => {
   const handleInputChanged = (event) => {
     const value = event.target.value;
 
     if (isNaN(value)) {
-      setErrorAlert('Value is not a number')
+
     } else if (value > 50) {
-      setErrorAlert('Maximum value is 50')
+
     } else if (value <= 0) {
-      setErrorAlert('Minimum value is 1')
+
     } else {
-      setErrorAlert('');
+
       setCurrentNOE(value);
     }
   };
